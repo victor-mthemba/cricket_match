e75eb71a6a6b0286e47546c9ea6a6c64c010d0e2
victor-mthemba <victormthemba@gmail.com>
2019-09-20 10:51:07 +0200

    my first commit

---
+++ test/cricket-keeper-test.js
@@ -0,0 +1,7 @@
+describe('The the score for one over', function(){
+
+    it('It should return total score is 9 if the string is "-2-3-4"', function(){
+        assert.equal("-2-3-4", 9);
+    });
+
+});
